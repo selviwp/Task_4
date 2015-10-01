e98a4186c18e30a2273db68a7c2b757328985b3f
selviwp <selviwidyapraditiap@yahoo.com>
2015-10-01 22:10:21 +0700

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -6,6 +6,22 @@
 #include "player.h"
 using namespace std;
 
+void play_music (address P)
+{
+    string filename;
+
+    filename=info(P).name;
+    cout<<"Playing "<<filename<<endl;
+    PlaySound(TEXT(filename.c_str()), NULL, SND_FILENAME);
+    _sleep(1000); //delay 1 second
+}
+
+void play_next (address &P)
+{
+    P=next(P);
+    play_music(P);
+}
+
 void play_prev (address &P)
 {
     P=prev(P);
